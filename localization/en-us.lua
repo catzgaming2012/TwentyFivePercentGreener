d328e89300073b7b2408b7b7f6c0db29d73cf925
catzgaming2012 <197011726+catzgaming2012@users.noreply.github.com>
2026-06-11 13:39:40 -0600

    m

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -10,7 +10,7 @@ return {
         },
         Joker = {
             j_tfpg_the_guy_from_that_meme_lathering_green_all_over_him = {
-                name = ("I'm so {C:tfpg_00ff00%s}G R E E N"):format(PassiveChaos and ",E:pc_wave" or ""),
+                name = ("I'm so {C:tfpg_00ff00%s}G R E E N"):format(PC and ",E:pc_wave" or ""),
                 text = {
                     "{C:mult}+#1#{} mult for each {X:tfpg_00ff00,C:white}#2#%{C:tfpg_00ff00} of green",
                     "{C:inactive,s:0.9}(Currently {C:mult,s:0.9}+#3#{C:inactive,s:0.9} mult)"
